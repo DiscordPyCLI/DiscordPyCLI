91a63e850aea3354b55b2cc805c6dd36db2ec198
Dan6erbond <moravrav@gmail.com>
2020-09-04 17:41:51 +0200

    Add basic command line argument for simple bot structures.

--- bot/bot.py
+++ bot/bot.py
@@ -9,7 +9,14 @@ parser.add_argument("args", help="First additional argument passed to the comman
 """
 Bot creation arguments:
 @ --cog Adds one or more cogs to the base bot structure.
+@ --basic Only instantiates a basic bot, does not create a class.
 """
+parser.add_argument(
+    "--basic",
+    help="Only instantiates a basic bot, does not create a class.",
+    nargs="?",
+    const=True,
+    default=False)
 parser.add_argument("--cog", help="Cogs to include in the bot.", nargs="*", action="extend")
 
 
