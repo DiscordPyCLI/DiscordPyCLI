d234cd7b9051309a0b2701c7ecf6b9d61104e2b3
Dan6erbond <moravrav@gmail.com>
2020-09-04 16:30:35 +0200

    Add case converter

---
+++ bot/utils.py
@@ -0,0 +1,13 @@
+import re
+
+def get_cases(source):
+    parts = source.split("-") if len(source.split("-")) > 1 else source.split("_")
+
+    if len(parts) > 1:
+        snake_case = "_".join(p.lower() for p in parts)
+        camel_case = "".join(p.title() for p in parts)
+    else:
+        snake_case = re.sub(r'(?<!^)(?=[A-Z])', '_', source).lower()
+        camel_case = "".join(p.title() for p in snake_case.split("_"))
+
+    return snake_case, camel_case
