05150ab22d09759bb56e28e316b57be0e3a68c29
Dan6erbond <moravrav@gmail.com>
2020-09-07 13:24:11 +0200

    Use `click` library to create commands

--- dpycli/dpy_cli.py
+++ dpycli/dpy_cli.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 
 import click
@@ -9,55 +8,38 @@ from .const import yaml
 from .discord_bot import DiscordBot
 from .utils import get_cases
 
-parser = argparse.ArgumentParser(description="Discord bot CLI scripts.")
-parser.add_argument("mode", help="One of 'create', 'add', or 'run'.", choices=["create", "add", "run"])
-parser.add_argument("args", help="First additional argument passed to the command.", nargs="*")
-
-"""Bot creation arguments:
-@ --cog Adds one or more cogs to the base bot structure.
-@ --basic Only instantiates a basic bot, does not create a class.
-"""
-parser.add_argument(
-    "--basic",
-    "-b",
-    help="Only instantiates a basic bot, does not create a class.",
-    nargs="?",
-    const=True,
-    default=False)
-parser.add_argument("--cog", "-c", help="Cogs to include in the bot.", nargs="*")
 
 @click.group()
 def main():
-    '''
-    args = parser.parse_args()
+    pass
 
-    if args.mode == "create":
-        if not args.args:
-            print("[ERROR:] The bot's name or a create-mode must be specified.")
-            return
-        elif args.args[0] == "cog":
-            config = DiscordBot().get_bot_config()
-            CogBuilder(
-                os.getcwd(),
-                os.path.basename(config["bot"]["main"]),
-                get_cases(config["name"])[1]
-            ).add_cog(args.args[1])
-        else:
-            BotBuilder(args.args[0], args.basic).create(args.cog)
-            '''
 
-@main.group()
-@click.argument("name", nargs=1, required=False)
-def create(name):
-    print("Create:", name)
+"""
+Bot creation arguments:
+@ name The name of the bot in either camel, snake or kebab case.
+@ --cog Adds one or more cogs to the base bot structure.
+@ --basic Only instantiates a basic bot, does not create a class.
 
-@create.command("*")
-@click.argument("name")
-def bot(name):
-    if name != "cog":
-        print("Bot:", name)
+Cog creation arguments:
+@ name The name of the cog in either camel, snake or kebab case.
+"""
 
-@create.command()
-@click.argument("name")
-def cog(name):
-    print("Cog:", name)
+
+@main.command()
+@click.argument("name", nargs=1, required=True)
+@click.option("--basic", "-b", default=False, is_flag=True,
+              help="Only instantiates a basic bot, does not create a class.")
+@click.option("--cog", "-c", help="Cogs to include in the bot.", multiple=True)
+@click.argument("cog-name", nargs=1, required=False)
+def create(name, basic, cog, cog_name):
+    if name == "cog" and cog_name:
+        config = DiscordBot().get_bot_config()
+        CogBuilder(
+            os.getcwd(),
+            os.path.basename(config["bot"]["main"]),
+            get_cases(config["name"])[1]
+        ).add_cog(cog_name)
+    elif name == "cog" and not cog_name:
+        print("Argument cog-name is missing!")
+    else:
+        BotBuilder(name, basic).create(cog)
